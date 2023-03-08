b6b2d3d24f1ae679393b375bec55ddb6521ad149
Ricardo Luquetti <99838772+RLC02@users.noreply.github.com>
2023-03-08 14:19:02 -0300

    Add files via upload

---
+++ Calculo-X-N/src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    val N = 35
+    val X = 40
+    val resultado = Math.pow(X*N.toDouble(), 2.toDouble())
+
+    println("calculo (N*X)^2")
+    println("Resultado = $resultado")
+}
