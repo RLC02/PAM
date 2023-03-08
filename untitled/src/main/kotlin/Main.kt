1c4265b1bb6ac382dfb6302a6875b3e4a1590779
Ricardo Luquetti <99838772+RLC02@users.noreply.github.com>
2023-03-08 14:20:12 -0300

    Add files via upload

---
+++ untitled/src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+
+    val base = 10
+    val altura = 15
+    val resultado = (base*altura)
+
+    println("Calculo da Area de Um Retangulo \n")
+    println("Resultado =  $resultado")
+
+
+}
