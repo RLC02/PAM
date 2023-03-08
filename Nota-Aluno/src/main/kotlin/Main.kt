b94d4d0a91a1f80157d4af970b82cc5df5460d8c
Ricardo Luquetti <99838772+RLC02@users.noreply.github.com>
2023-03-08 14:16:32 -0300

    Add files via upload

---
+++ Nota-Aluno/src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    val n1 = 10
+    val n2 = 8
+    val n3 = 6
+    val n4 = 7
+    val media = (n1+n2+n3+n4)/4
+
+    println("Calculo Media Aluno")
+    println("A media é = $media")
+}
