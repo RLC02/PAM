7ddcdd0dd8553e5948e942e498e9e8720f12f631
Ricardo Luquetti <99838772+RLC02@users.noreply.github.com>
2023-03-08 14:17:45 -0300

    Add files via upload

---
+++ Area-Triangulo/src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+   var base = 30
+   val altura =20
+   val resultado = (base*altura)/2
+
+   println("Calculo Area de Um Triangulo")
+   println("Resultado = $resultado")
+}
